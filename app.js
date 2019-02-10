8c527d09b8321762143cd4daf49e3fb87abf819e
jiefenn8 <jiefen8nz@gmail.com>
2019-02-11 01:15:58 +1300

    Removed cors configuration

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var graphRouter = require('./routes/graph');
-var searchRouter = require('./routes/search');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var graphRouter = require("./routes/graph");
+var searchRouter = require("./routes/search");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-var whitelist = [
-    'http://0.0.0.0:5500',
-];
-var corsOptions = {
-    origin: function(origin, callback){
-        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-        callback(null, originIsWhitelisted);
-    },
-    credentials: true
-};
-app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/graph', graphRouter);
-app.use('/search', searchRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/graph", graphRouter);
+app.use("/search", searchRouter);
 
 module.exports = app;
